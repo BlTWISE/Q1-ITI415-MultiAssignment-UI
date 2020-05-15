626537f1aeb41701bf858e431e5b19e49ccb918f
BlTWISE <52811202+BlTWISE@users.noreply.github.com>
2020-05-15 13:31:48 -0400

    Multi-Link

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    private int pickupsLeft = 750;
+    public Text countText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            pickupsLeft--;
+            countText.text = pickupsLeft + " cubes left";
+        }
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myPrefab;
+
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start()
+    {
+
+        Vector3 terrainSize = GameObject.Find("Terrain").GetComponent<Terrain>().terrainData.size;
+
+        for (int i = 0; i < 750; i++)
+        {
+            // Instantiate at random position and zero rotation.
+            Instantiate(myPrefab, new Vector3(Random.Range(0, terrainSize.x), 0.5f, Random.Range(0, terrainSize.z)), Quaternion.identity);
+        }
+        
+    }
+}
